6ee549492cb6752805c4810310c7c8d238a2fd80
JOY <debanshupanigrahi53605@gmail.com>
2024-09-02 19:07:56 +0530

    modified:   src/component/Details.js 	modified:   src/component/EditPage.js 	deleted:    src/route_manager/handlers/loader.js

--- src/component/EditPage.js
+++ src/component/EditPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const EditPage = () => {
-    
+
   return (
     <div>EditPage</div>
   )
--- src/route_manager/handlers/loader.js
+++
@@ -1,9 +0,0 @@
-import { getContactDetails } from "../../utils/renderContacts";
-import { Outlet, Link } from "react-router-dom";
-
- const  loader = () => {    // have not used yet
-    const contact =  getContactDetails();
-    return contact
-}
-
-export {loader}
